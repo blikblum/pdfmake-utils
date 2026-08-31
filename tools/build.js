a9c786b6559104827a053fb000deec623384eb76
Luiz Americo <blikblum@users.noreply.github.com>
2026-08-30 22:37:51 -0300

    Upgrade raw-element, remove babel

--- tools/build.js
+++ tools/build.js
@@ -2,7 +2,6 @@
 
 const del = require('del')
 const rollup = require('rollup')
-const babel = require('rollup-plugin-babel')
 const pkg = require('../package.json')
 
 let promise = Promise.resolve()
@@ -18,20 +17,23 @@ const formats = ['es']
 
 // Compile source code into a distributable format with Babel
 formats.forEach((format) => {
-  promise = promise.then(() => rollup.rollup({
-    input: 'src/index.js',
-    external: Object.keys(dependencies),
-    plugins: [babel({
-      exclude: 'node_modules/**'
-    })]
-  }).then(bundle => bundle.write({
-    file: `dist/${outputName}.js`,
-    format,
-    sourcemap: true
-  })))
+  promise = promise.then(() =>
+    rollup
+      .rollup({
+        input: 'src/index.js',
+        external: Object.keys(dependencies),
+      })
+      .then((bundle) =>
+        bundle.write({
+          file: `dist/${outputName}.js`,
+          format,
+          sourcemap: true,
+        })
+      )
+  )
 })
 
-promise.catch(err => {
+promise.catch((err) => {
   console.error(err.stack) // eslint-disable-line no-console
   process.exit(1)
 })
